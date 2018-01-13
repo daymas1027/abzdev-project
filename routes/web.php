f722a3423c8e463fb98c9826dd44d6c767e821a2
daymas1027 <a.docenko_1027@mail.ru>
2018-01-13 17:36:38 +0300

    test

--- routes/web.php
+++ routes/web.php
@@ -14,6 +14,6 @@
 Route::get('/', function () {
     return view('welcome');
 });
-Route::get('/test', function (){
-    return view('welcome');
+Route::get('test', function (){
+    return view('test');
 });
